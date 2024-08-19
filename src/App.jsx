5cf551e7841994736e06d80c48f26f55d5a781bc
Robert A Dixon <robmsc24@gmail.com>
2024-08-18 21:35:02 -0400

    Todo list

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+function App() {
+  return (
+    <main className="top-right">
+      <TodoInput />
+      <TodoList />
+    </main>
+  )
+}
+
+export default App
